459b8b512d147fab870e93c4cc782f7008476ede
Tholoana Manyane <tholoana.manyane@gmail.com>
2024-05-25 07:45:05 -0700

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,6 +14,21 @@ app.register_blueprint(app_views)
 def teardown_db(exception):
     storage.close()
 
+@app.errorhandler(404)
+def handle_404(exception):
+    """
+    handles 404 error
+    :return: returns 404 json
+    """
+    data = {
+        "error": "Not found"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 404
+
+    return(resp)
+
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', '5000'))
