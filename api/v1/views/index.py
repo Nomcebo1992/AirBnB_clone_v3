34e4d15d7816264d642d92a71443ab5fce48dd0f
Tholoana Manyane <tholoana.manyane@gmail.com>
2024-05-25 06:59:25 -0700

    index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,5 +14,12 @@ def get_status():
 
 @app.route('/api/v1/stats', methods=['GET'])
 def get_stats():
-    counts = storage.count()
+    counts = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
+            }
     return jsonify(counts)
