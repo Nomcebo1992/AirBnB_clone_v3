61d08790ea2d68fd51d9e2479577f25a5900ccc7
Tholoana Manyane <tholoana.manyane@gmail.com>
2024-05-25 06:55:52 -0700

    index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,17 @@
 """
 This module contains endpoint(route) status
 """
+
 from api.v1.views import app_views
 from flask import jsonify
+
+from models import storage
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
     return jsonify({"status": "OK"})
+
+@app.route('/api/v1/stats', methods=['GET'])
+def get_stats():
+    counts = storage.count()
+    return jsonify(counts)
