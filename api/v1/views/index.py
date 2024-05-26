2b58ad7ce84ad78ec25d51ee8d252c82d81f509f
Tholoana Manyane <tholoana.manyane@gmail.com>
2024-05-26 14:14:39 +0200

    adding changes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ This module contains endpoint(route) status
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status', methods=['GET'])
 def get_status():
     """Returns the status of the API"""
