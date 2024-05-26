8539708e67b02d548ecd2582f740612a43849e45
Nomcebo1992 <snembambo@gmail.com>
2024-05-26 03:20:42 -0700

    index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,29 +5,7 @@ This module contains endpoint(route) status
 from api.v1.views import app_views
 from flask import jsonify
 
-from models import storage
-
 @app_views.route('/status', methods=['GET'])
 def get_status():
-    """
-    Status Route
-    return: Response with JSON
-    """
+    """Returns the status of the API"""
     return jsonify({"status": "OK"})
-
-@app.route('/stats', methods=['GET'])
-def get_stats():
-    """
-    stats of all obj routes
-    """
-    data = {
-            "amenities": storage.count("Amenity"),
-            "cities": storage.count("City"),
-            "places": storage.count("Place"),
-            "reviews": storage.count("Review"),
-            "states": storage.count("State"),
-            "users": storage.count("User"),
-            }
-
-    return jsonify(data)
-
