59b044e20df868e13715377a1de9dd3751e9f5b0
Tholoana Manyane <tholoana.manyane@gmail.com>
2024-05-26 14:30:34 +0200

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,10 +17,19 @@ def teardown_db(exception):
     storage.close()
 
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    """Closes storage on teardown"""
-    storage.close()
+@app.errorhandler(404)
+def error_404(error):
+    '''Handles the 404 HTTP error code.'''
+    return jsonify(error='Not found'), 404
+
+
+@app.errorhandler(400)
+def error_400(error):
+    '''Handles the 400 HTTP error code.'''
+    msg = 'Bad request'
+    if isinstance(error, Exception) and hasattr(error, 'description'):
+        msg = error.description
+    return jsonify(error=msg), 400
 
 
 if __name__ == "__main__":
