17087abc1cf9b3443890bb2768765da2a8f2ac0f
Nomcebo1992 <snembambo@gmail.com>
2024-05-26 02:41:42 -0700

    index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python3
 """
-This module contains the principal application
+This module contains endpoint(route) status
 """
-from flask import Flask, jsonify
 from api.v1.views import app_views
+from flask import jsonify
+
 from models import storage
 
-@app_views.route('/api/v1/stats', methods=['GET'])
+@app_views.route('/status', methods=['GET'])
+def get_status():
+    """
+    Status Route
+    return: Response with JSON
+    """
+    return jsonify({"status": "OK"})
+
+@app.route('/stats', methods=['GET'])
 def get_stats():
-    """Retrieves the number of each object by type"""
-    stats = {
-        'amenities': storage.count('Amenity'),
-        'cities': storage.count('City'),
-        'places': storage.count('Place'),
-        'reviews': storage.count('Review'),
-        'states': storage.count('State'),
-        'users': storage.count('User')
-    }
-    return jsonify(stats)
+    """
+    stats of all obj routes
+    """
+    data = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User"),
+            }
+
+    return jsonify(data)
+
