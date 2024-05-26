417605f816764237581aee266ae72d490726803b
Tholoana Manyane <tholoana.manyane@gmail.com>
2024-05-26 14:19:49 +0200

    checker

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,3 +10,16 @@ from flask import jsonify
 def get_status():
     """Returns the status of the API"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def stat():
+    """returns the number of each objects by type"""
+    return jsonify(
+        amenities=storage.count('Amenity'),
+        cities=storage.count('City'),
+        places=storage.count('Place'),
+        reviews=storage.count('Review'),
+        states=storage.count('State'),
+        users=storage.count('User')
+    )
